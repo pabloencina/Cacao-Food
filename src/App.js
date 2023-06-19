b6d1d3854bedde346fe6a2e4a6148ac4b119eaab
Fede Villarreal <fedevillarreal94@gmail.com>
2023-06-19 20:14:51 -0300

    Estructura inicial de las paginas home, nosotros y clientes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Styles/App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Nosotros from './pages/Nosotros';
+import Clientes from './pages/Clientes';
+
 function App() {
-  return <div className="App">Hola Mundo</div>;
+  return (
+    <BrowserRouter>
+      <header>
+        {/* NavBar */}
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path='/clientes' element={<Clientes />} />
+        </Routes>
+      </main>
+      <footer>
+        {/* footer */}
+      </footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;

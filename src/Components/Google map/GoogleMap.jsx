4c60d9db6fe90a0c9c1b137d8021a7985fa5ee18
Pablo <encina.pd@gmail.com>
2023-06-23 08:42:54 -0300

    Se trabaja en la inserción de google map en la sección de Contactanos

---
+++ src/Components/Google map/GoogleMap.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+
+const GoogleMap = (props) => {
+  const mapStyles = {
+    width: "70%",
+    height: "30rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginTop: "2rem",
+  };
+  return (
+    <Map
+      google={props.google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={{
+        lat: 37.7749,
+        lng: -122.4194,
+      }}
+    />
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: "",
+})(GoogleMap);

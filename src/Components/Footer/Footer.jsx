4c3692a1744c1c08c572854e1c8663c76eaec65a
Pablo <encina.pd@gmail.com>
2023-07-11 18:21:14 -0300

    Se realizan cambios de estilos en el Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Facebook1 from "../../Images/facebook (1).svg";
+import Instagram from "../../Images/instagram-svgrepo-com.svg";
 
 import { Link } from "react-router-dom";
 
@@ -6,8 +8,22 @@ const Footer = () => {
   return (
     <div className="footer__container">
       <div className="footer__container_social_media">
-        <Link className="footer__social_media_Instagram">CacaoFood</Link>
-        <Link className="footer__social_media_Facebook">CacaoFood</Link>
+        <Link className="footer__social_media_Instagram">
+          <img
+            src={Instagram}
+            alt="Instagram"
+            className="footer__image_Facebook"
+          />
+          CacaoFood
+        </Link>
+        <Link className="footer__social_media_Facebook">
+          <img
+            src={Facebook1}
+            alt="Facebook"
+            className="footer__image_Facebook"
+          />
+          CacaoFood
+        </Link>
       </div>
     </div>
   );

16b9087e44f82c17acc9e227af29ce7a3833b720
Pablo <encina.pd@gmail.com>
2023-07-10 21:19:43 -0300

    Diseño UI section Card Menu

--- src/Components/Menu/MenuSections.jsx
+++ src/Components/Menu/MenuSections.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../Css/active.css";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MenuSections = () => {
   return (

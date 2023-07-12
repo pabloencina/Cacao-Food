56a5eb2b90952b951e6ec6562c994cfdd1adac8e
Pablo <encina.pd@gmail.com>
2023-07-12 13:31:38 -0300

    Se realizan los hovers de los buttons y lins correspondientes a la UI

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-import Facebook1 from "../../Images/facebook (1).svg";
-import Instagram from "../../Images/instagram-svgrepo-com.svg";
-
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <div className="footer__container">
       <div className="footer__container_social_media">
-        <Link className="footer__social_media_Instagram">
-          <img
-            src={Instagram}
-            alt="Instagram"
-            className="footer__image_Facebook"
-          />
+        <Link
+          title="Instagram CacaoFood"
+          className="footer__social_media_Instagram"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
           CacaoFood
         </Link>
-        <Link className="footer__social_media_Facebook">
-          <img
-            src={Facebook1}
-            alt="Facebook"
-            className="footer__image_Facebook"
-          />
+        <Link
+          title="Facebook CacaoFood"
+          className="footer__social_media_Facebook"
+        >
+          <FontAwesomeIcon icon={faFacebookF} />
           CacaoFood
         </Link>
       </div>

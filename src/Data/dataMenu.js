29d84cf58e76d52257586866327618a258ff0ee5
Pablo <encina.pd@gmail.com>
2023-07-13 09:11:33 -0300

    Se trabaja en el JSON, modificando los nombres y descrpción de los platos

--- src/Data/dataMenu.js
+++ src/Data/dataMenu.js
@@ -3,90 +3,207 @@ import chickenImage from "../Images/imageChicken.jpg";
 const dataMenu = [
   {
     id: 1,
-    name: "Pollo a la plancha con ensalada mixta",
+    name: "Verduras Salteadas",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "Este plato saludable resalta los sabores naturales de las verduras, manteniendo su textura crujiente.Puede servirse como guarnición o como plato principal.",
     price: 3000,
     image: { chickenImage },
-    classification: "postres",
+    classification: "ensalada",
   },
   {
     id: 2,
-    name: "Pollo con pure mixta",
+    name: "Ensalada César",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qureiciendis, recusandae cum quisquam facilis ipsam deserunt, corporisoptio perferendis dolores est doloribus soluta illum, totam repellendus aliquam nulla magnam. Fugit?",
+      "La ensalada César es una clásica mezcla de lechuga romana, crutones, queso parmesano y aderezo César. Es famosa por su sabor distintivo y texturas variadas.",
     price: 3000,
     image: { chickenImage },
     classification: "ensalada",
   },
   {
     id: 3,
-    name: "Pollo a la plancha con ensalada mixta",
-    description: "El pollo a la plancha con ensalada mixta.",
+    name: "Puré de Papas",
+    description:
+      "Es perfecto para complementar platos de carnes, aves o pescado, y su sabor sencillo y delicioso lo convierte en un favorito en muchas mesas.",
     price: 3000,
     image: { chickenImage },
     classification: "ensalada",
   },
   {
     id: 4,
-    name: "Pollo a la plancha con ensalada mixta",
+    name: "Ensalada Griega",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "La ensalada griega es una fresca y sabrosa combinación de ingredientes mediterráneos. Destacada por su equilibrio de sabores y colores.",
     price: 3000,
     image: { chickenImage },
-    classification:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+    classification: "ensalada",
   },
   {
     id: 5,
-    name: "Pollo a la plancha con ensalada mixta",
+    name: "Ensalada Clásica",
     description:
       "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
     price: 3000,
     image: { chickenImage },
-    classification: "cremas",
+    classification: "ensalada",
   },
   {
     id: 6,
-    name: "Pollo a la plancha con ensalada mixta",
+    name: "Ensalada de la Casa",
     description:
       "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
     price: 3000,
     image: { chickenImage },
-    classification: "cremas",
+    classification: "ensalada",
   },
   {
     id: 7,
-    name: "Pollo a la plancha con ensalada mixta",
+    name: "Torta de Zanahorias",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "Nuestra torta de zanahoria es una delicia esponjosa y saludable. Con su mezcla perfecta de zanahorias ralladas, nueces y especias, cada bocado te brinda una explosión de sabores naturales.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "postres",
+  },
+  {
+    id: 8,
+    name: "Torta de Frutos rojos y Almendras",
+    description:
+      "Esta torta es una exquisitez saludable. Cada bocado combina la dulzura de los frutos rojos con la textura crujiente de las almendras.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "postres",
+  },
+  {
+    id: 9,
+    name: "Torta de Banana",
+    description:
+      "Su suave textura y sabor naturalmente dulce conquistaran en cada bocado. Disfruta de este postre nutritivo que combina la bondad de las bananas con un toque de esponjosidad.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "postres",
+  },
+  {
+    id: 10,
+    name: "Torta de Limón",
+    description:
+      "Nuestra torta de limón es una explosión refrescante de sabores, donde cada bocado te transporta a un paraíso cítrico y ligero.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "postres",
+  },
+  {
+    id: 11,
+    name: "Torta de Naranjas y Coco",
+    description:
+      "Esta torta te transporta a playas exóticas con su sabor refrescante y textura suave. Disfruta de este postre saludable que te hará sentir como si estuvieras en un paraíso culinario.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "postres",
+  },
+  {
+    id: 12,
+    name: "Crema de Zanahoria",
+    description:
+      "Disfruta de esta opción ligera y deliciosa, cuya textura suave y sabor delicado se convierte en una explosión de nutrientes",
     price: 3000,
     image: { chickenImage },
     classification: "cremas",
   },
   {
-    id: 8,
-    name: "Pollo a la plancha con ensalada mixta",
+    id: 13,
+    name: "Crema de Coliflor",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "Incorpora más vegetales a tu dieta con esta opción cremosa y nutritiva. Es un plato saludable y confortante que te nutrirá y satisfará.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "cremas",
+  },
+  {
+    id: 14,
+    name: "Crema de Espinacas",
+    description:
+      "Sumérgete en la deliciosa suavidad de nuestra crema de espinacas. Cada cucharada te envolverá en un sabor verde y confortante, lleno de nutrientes y frescura.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "cremas",
+  },
+  {
+    id: 15,
+    name: "Crema de Calabaza",
+    description:
+      "Nuestra crema de calabaza es una explosión de sabor y confort. Con su textura suave y su sabor reconfortante, esta crema te envolverá en un abrazo cálido y nutritivo.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "cremas",
+  },
+  {
+    id: 16,
+    name: "Crema de Brocoli",
+    description:
+      "Disfruta de esta opción nutritiva y deliciosa que te brinda una dosis de verduras en cada bocado, con la delicadeza de brocoli fresco.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "cremas",
+  },
+  {
+    id: 17,
+    name: "Limonada",
+    description:
+      "Refrescante y revitalizante. Disfruta de esta bebida saludable y llena de vitamina C que te hidrata y revitalizará.",
     price: 3000,
     image: { chickenImage },
     classification: "bebidas",
   },
   {
-    id: 9,
-    name: "Pollo a la plancha con ensalada mixta",
+    id: 18,
+    name: "Naranjada",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "El jugo fresco de naranjas recién exprimidas te brinda una sensación naturalmente dulce y refrescante. Disfruta dfe esta bebida llena de vitamina C que te revitalizará en cada sorbo.",
     price: 3000,
     image: { chickenImage },
     classification: "bebidas",
   },
   {
-    id: 10,
-    name: "Pollo a la plancha con ensalada mixta",
+    id: 19,
+    name: "Jugo de Mandarina",
     description:
-      "El pollo a la plancha con ensalada mixta es una deliciosa opción de comida saludable y nutritiva. Este plato combina la jugosidad y el sabor del pollo a la plancha con una refrescante ensalada de vegetales variados.",
+      "Esta bebida saludable te hidrata y revitaliza, transportándote en cada sorbo a un oasis de dulzura natural y vitamina C.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "bebidas",
+  },
+  {
+    id: 20,
+    name: "Jugo Guanabana",
+    description:
+      "Esta bebida saludable te brinda un festín de antioxidantes y vitaminas, con su sabor dulce y refrescante.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "bebidas",
+  },
+  {
+    id: 21,
+    name: "Jugo de Fresa",
+    description:
+      "Esta bebida saludable que te brinda una explosión de sabor y beneficios antioxidantes, donde cada sorbo te deleitará con la intensidad y dulzura de las fresas maduras.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "bebidas",
+  },
+  {
+    id: 22,
+    name: "Jugo de Mora",
+    description:
+      "Nuestro jugo de mora es una sinfonia de sabores intensos y antioxidantes. Cada sorbo te sumerge en la dulzura y frescura de las moras silvestres.",
+    price: 3000,
+    image: { chickenImage },
+    classification: "bebidas",
+  },
+  {
+    id: 23,
+    name: "Jugo de Lulo y Maracuya",
+    description:
+      "Este delicioso jugo natural te transporta a un paraíso tropical con un equilibrio perfecto de sabores y tropicales.",
     price: 3000,
     image: { chickenImage },
     classification: "bebidas",

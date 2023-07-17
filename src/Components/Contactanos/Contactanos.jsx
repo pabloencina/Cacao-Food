864d5d0952e51cafd6f81ceff9e985bd1fa6a330
Pablo <encina.pd@gmail.com>
2023-07-17 11:26:57 -0300

    Se trabaja en la page Contactanos, se instala e integra FORMIK para los formularios.

--- src/Components/Contactanos/Contactanos.jsx
+++ src/Components/Contactanos/Contactanos.jsx
@@ -4,8 +4,14 @@ import Facebook from "../../Images/facebook.svg";
 import WhatsApp from "../../Images/whatsapp.svg";
 import { Button, NavLink } from "react-bootstrap";
 import GoogleMap from "../Google-map/GoogleMap";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+//import "../../Css/error.css";
 
 const Contactanos = () => {
+  const handleSubmit = (values) => {
+    console.log(values); // Hacer algo con los valores del formulario
+  };
+
   return (
     <div>
       <div>
@@ -19,7 +25,7 @@ const Contactanos = () => {
             <div>
               <div className="contactanos__text_box">
                 <p className="contactanos__text">
-                  Puedes comunicarte con nosotros a traves de nuestras redes
+                  Puedes comunicarte con nosotros a través de nuestras redes
                   sociales:
                 </p>
               </div>
@@ -60,54 +66,95 @@ const Contactanos = () => {
                     O déjanos un comentario en el siguiente formulario:
                   </p>
                 </div>
-                <div className="contactanos__text_input_box">
-                  <p className="contactanos__text_input">
-                    Nombre y apellido
-                    <span className="contactanos__asterisco">*</span>
-                  </p>
-                </div>
-
-                <div>
-                  <input
-                    className="contactanos__input_box"
-                    placeholder="   Ingresa tu nombre: Ej Maria Perez"
-                    required
-                    type="text"
-                  />
-                </div>
-                <div className="contactanos__text_input_box">
-                  <p className="contactanos__text_input">
-                    Correo Electrónico{" "}
-                    <span className="contactanos__asterisco">*</span>
-                  </p>
-                </div>
-                <div>
-                  <input
-                    className="contactanos__input_box"
-                    placeholder="  Ingresa tu correo electrónico: maria_perez@gmail.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <div className="contactanos__text_input_box">
-                    <p className="contactanos__text_input">
-                      Mensaje<span className="contactanos__asterisco">*</span>
-                    </p>
-                  </div>
-                  <div>
-                    <textarea
-                      className="contactanos__text_area_box"
-                      placeholder="   Ingresa tu reseña"
-                      required
-                      type="text"
-                    />
-                  </div>
-                </div>
-                <div className="contactanos__box_button_enviar">
-                  <Button className="contactanos__button_enviar">
-                    Enviar Formulario
-                  </Button>
-                </div>
+                <Formik
+                  initialValues={{
+                    name: "",
+                    email: "",
+                    message: "",
+                  }}
+                  validate={(values) => {
+                    const errors = {};
+                    if (!values.name) {
+                      errors.name = "Este campo es obligatorio";
+                    }
+                    if (!values.email) {
+                      errors.email = "Ingresa un correo electrónico válido";
+                    }
+                    if (!values.message) {
+                      errors.message = "Este campo es obligatorio";
+                    }
+                    return errors;
+                  }}
+                  onSubmit={handleSubmit}
+                >
+                  <Form>
+                    <div className="contactanos__text_input_box">
+                      <p className="contactanos__text_input">
+                        Nombre y apellido
+                        <span className="contactanos__asterisco">*</span>
+                      </p>
+                    </div>
+                    <div>
+                      <Field
+                        className="contactanos__input_box"
+                        placeholder="   Ingresa tu nombre: Ej Maria Perez"
+                        name="name"
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="div"
+                        className="error"
+                      />
+                    </div>
+                    <div className="contactanos__text_input_box">
+                      <p className="contactanos__text_input">
+                        Correo Electrónico{" "}
+                        <span className="contactanos__asterisco">*</span>
+                      </p>
+                    </div>
+                    <div>
+                      <Field
+                        className="contactanos__input_box"
+                        placeholder="  Ingresa tu correo electrónico: maria_perez@gmail.com"
+                        name="email"
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="error"
+                      />
+                    </div>
+                    <div>
+                      <div className="contactanos__text_input_box">
+                        <p className="contactanos__text_input">
+                          Reseña
+                          <span className="contactanos__asterisco">*</span>
+                        </p>
+                      </div>
+                      <div>
+                        <Field
+                          as="textarea"
+                          className="contactanos__text_area_box"
+                          placeholder="   Ingresa tu reseña"
+                          name="message"
+                        />
+                        <ErrorMessage
+                          name="message"
+                          component="div"
+                          className="error"
+                        />
+                      </div>
+                    </div>
+                    <div className="contactanos__box_button_enviar">
+                      <Button
+                        type="submit"
+                        className="contactanos__button_enviar"
+                      >
+                        Enviar Formulario
+                      </Button>
+                    </div>
+                  </Form>
+                </Formik>
               </div>
             </div>
           </div>
@@ -122,10 +169,9 @@ const Contactanos = () => {
               </div>
               <div className="contactanos__text_box">
                 <p className="contactanos__text">
-                  Encontranos en estas direcciones:
+                  Encuéntranos en estas direcciones:
                 </p>
               </div>
-
               <div>
                 <div className="contactanos__google_map_container">
                   <GoogleMap />

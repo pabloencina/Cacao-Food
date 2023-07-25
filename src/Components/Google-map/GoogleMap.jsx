0cfde1bc8cef270ade005ac0c3837deb5c467340
Pablo <encina.pd@gmail.com>
2023-07-25 19:54:21 -0300

    Se trabaja en la instalacion Google maps

--- src/Components/Google-map/GoogleMap.jsx
+++ src/Components/Google-map/GoogleMap.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const GoogleMap = (props) => {
+  const defaultProps = {
+    center: {
+      lat: 10.60948264057215,
+      lng: -67.00308642767591,
+    },
+    zoom: 11,
+  };
   const mapStyles = {
     height: "27.68rem",
     width: "75.87rem",
     border: "1px solid #ccc",
     borderRadius: "4px",
-    //marginTop: "2rem",
   };
   return (
-    <Map
-      google={props.google}
-      zoom={14}
-      style={mapStyles}
-      initialCenter={{
-        lat: 10.60948264057215,
-        lng: -67.00308642767591,
-      }}
-    />
+    <div style={mapStyles}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent />
+      </GoogleMapReact>
+    </div>
   );
 };
 
-export default GoogleApiWrapper({
-  apiKey: "",
-})(GoogleMap);
+export default GoogleMap;
+
+//lat: 10.60948264057215,
+//lng: -67.00308642767591,
